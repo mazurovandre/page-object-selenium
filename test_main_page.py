08e779273190c3ffdc418d5ff7967eadaee5d03c
Mazurov Andrey <mazurovandre@gmail.com>
2020-10-12 22:08:20 +0300

    Add first test Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
-from conftest import browser
-
+from pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -10,3 +9,9 @@ def go_to_login_page(browser):
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     go_to_login_page(browser)
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
