35d38feb33b99ed98619e5d3e2a5fbcc48f4705d
Mazurov Andrey <mazurovandre@gmail.com>
2020-10-13 12:51:11 +0300

    Updated transitions between pages. test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,20 @@
+from pages.locators import MainPageLocators
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
